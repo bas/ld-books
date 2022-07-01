779f56390c576408cd44fb696f4ed503a49aaaea
Bas Peters <bas@github.com>
2022-07-01 12:50:58 +0200

    Configure feature flagging

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "@primer/react";
+import { withLDProvider } from "launchdarkly-react-client-sdk";
 
-function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
-}
+};
 
-export default App;
+export default withLDProvider({ clientSideID: process.env.NEXT_PUBLIC_LD_CLIENT_SIDE_ID })(
+  App
+);
